de801e5006985def4a7cbb104c49e25176f988d1
unknown <pratik110301@gmail.com>
2026-03-09 13:28:18 +0530

    Deployed frontend integrated

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,10 @@
 chrome.sidePanel
+  .setOptions({
+    path: "sidepanel.html",
+    enabled: true
+  })
+  .catch((error: Error) => console.error(error));
+
+chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error: Error) => console.error(error));
